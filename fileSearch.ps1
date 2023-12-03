11bcf90fc8959d0832dabc05a7ff583662fe6a37
adenning2 <adenning2@byui.edu>
2023-12-02 18:12:39 -0600

    Cleaned up script.

--- fileSearch.ps1
+++ fileSearch.ps1
@@ -1,21 +1,21 @@
-# Re-run the script as administrator
+# Administrator Elevation
 if(!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
     Start-Process powershell.exe -ArgumentList ("-NoProfile -ExecutionPolicy Bypass -File `"{0}`"" -f $PSCommandPath) -Verb RunAs
     Exit
 }
 
-# Prompt for the directory to search
-Write-Host "Enter the directory to search (leave blank for current directory): " -NoNewline -ForegroundColor Green -BackgroundColor Black
+# Directory Prompt
+Write-Host "Enter the directory to search (leave blank for current directory): " -NoNewline
 $searchDirectory = Read-Host
 if ([string]::IsNullOrWhiteSpace($searchDirectory)) {
     $searchDirectory = $PSScriptRoot
 }
 
-# Prompt for word in file name
-Write-Host "Input a filename search term: " -NoNewline -ForegroundColor Green -BackgroundColor Black
-$wordInput = Read-Host
+# Search Prompt
+Write-Host "Input a filename search term: " -NoNewline
+$searchTerm = Read-Host
 
-# Function to search for files recursively
+# Search
 function Search-Files {
     param(
         [Parameter(Mandatory = $true)] [string] $path,
@@ -24,53 +24,48 @@ function Search-Files {
     $parentPathLength = ($path -split '\\').Count
     $results = @()
 
-    # Searching in the parent directory
-    Write-Host "Searching in: $path" -ForegroundColor Yellow -BackgroundColor Black
+    # Parent Directory
+    Write-Host "Searching in: $path" -ForegroundColor Yellow 
     Get-ChildItem -Path $path -ErrorAction SilentlyContinue | ForEach-Object {
         if ($_.Name -like "*$findString*") {
-            Write-Host "Found: $($_.FullName)" -ForegroundColor Green -BackgroundColor Black
+            Write-Host "Found: $($_.FullName)" -ForegroundColor Green 
             $results += $_.FullName
         }
     }
 
-    # Searching in subdirectories
+    # Subdirectories
     Get-ChildItem -Path $path -Directory -ErrorAction SilentlyContinue | ForEach-Object {
         $currentPathLength = ($_.FullName -split '\\').Count
         if ($currentPathLength -eq $parentPathLength + 1) {
-            Write-Host "Searching in: $($_.FullName)" -ForegroundColor Yellow -BackgroundColor Black
+            Write-Host "Searching in: $($_.FullName)" -ForegroundColor Yellow 
             Get-ChildItem -Path $_.FullName -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
                 if ($_.Name -like "*$findString*") {
-                    Write-Host "Found: $($_.FullName)" -ForegroundColor Green -BackgroundColor Black
+                    Write-Host "Found: $($_.FullName)" -ForegroundColor Green 
                     $results += $_.FullName
                 }
             }
         }
     }
-    
-        if ($_.Name -like "*$findString*") {
-            Write-Host "Found: $($_.FullName)" -ForegroundColor Green -BackgroundColor Black
-            $results += $_.FullName
-        }
 
     return $results
 }
 
-# Perform the search
-$searchResults = Search-Files -path "$searchDirectory" -findString "$wordInput"
+# Perform Search
+$searchResults = Search-Files -path "$searchDirectory" -findString "$searchTerm"
 
-# Ask to save results
-Write-Host "Do you want to save the search results to a file? (y/n): " -NoNewline -ForegroundColor Green -BackgroundColor Black
+# Save
+Write-Host "Do you want to save the search results to a file? (y/n): " -NoNewline 
 $saveResponse = Read-Host
 if ($saveResponse -eq 'y') {
-    Write-Host "Enter the file path to save the results (leave blank for current directory, fileSearch.txt): " -NoNewline -ForegroundColor Green -BackgroundColor Black
+    Write-Host "Enter the file path to save the results (leave blank for current directory, fileSearch.txt): " -NoNewline 
     $filePath = Read-Host
     if ([string]::IsNullOrWhiteSpace($filePath)) {
         $filePath = "$PSScriptRoot\fileSearch.txt"
     }
     $searchResults | Out-File -FilePath $filePath
-    Write-Host "Results saved to $filePath" -ForegroundColor Green -BackgroundColor Black
+    Write-Host "Results saved to $filePath" -ForegroundColor Green 
 }
 
-# End of script
-Write-Host "Search completed. Press any key to exit." -ForegroundColor Green -BackgroundColor Black
+# Exit
+Write-Host "Search completed. Press any key to exit." -NoNewline
 $null = $Host.UI.RawUI.ReadKey("NoEcho, IncludeKeyDown")
